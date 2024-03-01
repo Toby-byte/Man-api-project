3a83bc70bc9c4ae2d9f89f28bb0d91f9c1e4c397
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-03-01 13:03:12 +0100

    mapbox done

--- api project/js/app2.js
+++ api project/js/app2.js
@@ -2,7 +2,8 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     const city_from_btn = document.getElementById("input-city").value;
 
     city = city_from_btn;
-    api_key = "api key here";
+    api_key_from_openweather = "api key here";
+    access_token = "api key here";
     mode = "html";
     units = "metric";
     lang = "en";
@@ -13,27 +14,22 @@ document.getElementById("submitBtn").addEventListener("click", function() {
         redirect: "follow"
     };
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api_key}`, fetchCoordinates)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api_key_from_openweather}`, fetchCoordinates)
         .then((response) => response.json()) // Assuming the API returns JSON. Use response.text() if it returns plain text.
         .then((result) => {
             const latitude = result[0].lat; // Accessing latitude inside the promise chain
             console.log(latitude); // This will log the latitude
             const lontitude = result[0].lon; // Accessing latitude inside the promise chain
             console.log(lontitude); // This will log the longitude
+            
+            // Create a new image element
+            let image = document.createElement("img");
 
+            // Set the source, width, and height of the image
+            image.src = `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${lontitude},${latitude},9,0/300x200?access_token=${access_token}`;
 
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${lontitude}&appid=${api_key}&mode=${mode}&units=${units}&lang=${lang}`, requestOptions)
-            .then((response) => response.text())
-            .then((result) => {
-                const city_name = document.createElement('h1');
-                city_name.innerHTML = city;
-                document.body.appendChild(city_name);
-                const div = document.createElement('div');
-                div.innerHTML = result;
-                document.body.appendChild(div);
-            })
-            .catch((error) => console.error(error));
-
+            // Append the image to the body of the document
+            document.body.appendChild(image);
 
         })
         .catch((error) => console.error(error));
--- readme.txt
+++ readme.txt
@@ -1,7 +1,11 @@
 Used API'S in this project:
 
 https://openweathermap.org/current
+https://home.openweathermap.org/api_keys
 
 https://docs.mapbox.com/api/maps/static-images/
+
+create requests:
+https://docs.mapbox.com/playground/static/
 
 https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
