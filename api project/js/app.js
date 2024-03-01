c53d2e03aaba9ed0e42a015fe1b2f23d1ae94431
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-03-01 11:28:32 +0100

    Weather api part done

--- api project/js/app.js
+++ api project/js/app.js
@@ -1,18 +1,45 @@
-city = "london";
-api_key = "ae611b27c9113372b836a7eec4e911b0";
+document.getElementById("submitBtn").addEventListener("click", function() {
+    const city_from_btn = document.getElementById("input-city").value;
 
-const fetchCoordinates = {
-    method: "GET",
-    redirect: "follow"
-};
+    city = city_from_btn;
+    api_key = "ae611b27c9113372b836a7eec4e911b0";
+    mode = "html";
+    units = "metric";
+    lang = "en";
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api_key}`, fetchCoordinates)
-    .then((response) => response.json()) // Assuming the API returns JSON. Use response.text() if it returns plain text.
-    .then((result) => {
-        const latitude = result[0].lat; // Accessing latitude inside the promise chain
-        console.log(latitude); // This will log the latitude
-        const lontitude = result[0].lon; // Accessing latitude inside the promise chain
-        console.log(lontitude); // This will log the longitude
-    })
-    .catch((error) => console.error(error));
 
+    const fetchCoordinates = {
+        method: "GET",
+        redirect: "follow"
+    };
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api_key}`, fetchCoordinates)
+        .then((response) => response.json()) // Assuming the API returns JSON. Use response.text() if it returns plain text.
+        .then((result) => {
+            const latitude = result[0].lat; // Accessing latitude inside the promise chain
+            console.log(latitude); // This will log the latitude
+            const lontitude = result[0].lon; // Accessing latitude inside the promise chain
+            console.log(lontitude); // This will log the longitude
+
+
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${lontitude}&appid=${api_key}&mode=${mode}&units=${units}&lang=${lang}`, requestOptions)
+            .then((response) => response.text())
+            .then((result) => {
+                const city_name = document.createElement('h1');
+                city_name.innerHTML = city;
+                document.body.appendChild(city_name);
+                const div = document.createElement('div');
+                div.innerHTML = result;
+                document.body.appendChild(div);
+            })
+            .catch((error) => console.error(error));
+
+
+        })
+        .catch((error) => console.error(error));
+
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+});
